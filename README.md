78e727828a3526d95867ba1faa165735939b5fa8
Bernardo <bernardo@partyou.com.br>
2020-08-25 23:47:47 -0300

    changing docker login to password-stdin. changing version package. changing readme. :ship:

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 ## NodeJS + Travis-CI + Heroku Deployment
+[![Build Status](https://travis-ci.org/bernardopadua/njs-deploy-travis.svg?branch=master)](https://travis-ci.org/bernardopadua/njs-deploy-travis)
 
 Simple node project using express and travis-ci for deployment in Heroku.
 Just for fun and learning.
--- scripts/docker.build.push.sh
+++ scripts/docker.build.push.sh
@@ -6,7 +6,7 @@ app="njs-deploy-travis"
 release="web"
 docker_image="registry.heroku.com"
 
-docker login -u="$HEROKU_LOGIN" -p="$HEROKU_PASS" registry.heroku.com
+echo "$HEROKU_PASS" | docker login -u="$HEROKU_LOGIN" --password-stdin registry.heroku.com
 
 docker build -t "${docker_image}/${app}/${release}" -f ./docker/Dockerfile.web .
 docker push "${docker_image}/${app}/${release}"
